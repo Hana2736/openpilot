9d6240e39b4953175b666fd266f9c10f65790ad8
James <91348155+FrogAi@users.noreply.github.com>
2025-11-29 12:00:00 -0700

    November 29th, 2025 Patch

--- frogpilot/ui/qt/widgets/frogpilot_controls.cc
+++ frogpilot/ui/qt/widgets/frogpilot_controls.cc
@@ -1,10 +1,6 @@
-#include <QFileInfo>
-#include <QJsonDocument>
-#include <QJsonObject>
-#include <QMovie>
-#include <QRegularExpression>
-
 #include "selfdrive/ui/ui.h"
+
+#include "frogpilot/ui/frogpilot_ui.h"
 
 bool FrogPilotConfirmationDialog::toggleReboot(QWidget *parent) {
   ConfirmationDialog d(tr("Reboot required to take effect."), tr("Reboot Now"), tr("Reboot Later"), false, parent);
@@ -17,51 +13,46 @@ bool FrogPilotConfirmationDialog::yesorno(const QString &prompt_text, QWidget *p
 }
 
 bool useKonikServer() {
-  static const bool use_konik = QFile::exists("/cache/use_konik");
+  static bool use_konik = QFile::exists("/cache/use_konik");
   return use_konik;
 }
 
-void loadGif(const QString &gifPath, QSharedPointer<QMovie> &movie, const QSize &size, QWidget *parent) {
-  if (!movie.isNull()) {
-    QObject::disconnect(movie.data(), nullptr, parent, nullptr);
-
+void clearMovie(QSharedPointer<QMovie> &movie, QWidget *parent) {
+  if (movie) {
+    QObject::disconnect(movie.data(), &QMovie::frameChanged, parent, nullptr);
     movie->stop();
-    movie.clear();
+    movie.reset();
   }
+}
+
+void loadGif(const QString &gifPath, QSharedPointer<QMovie> &movie, const QSize &size, QWidget *parent) {
+  clearMovie(movie, parent);
+
+  movie = QSharedPointer<QMovie>::create(gifPath, QByteArray(), parent);
+  movie->setCacheMode(QMovie::CacheAll);
+  movie->setScaledSize(size);
+
+  QObject::connect(movie.data(), &QMovie::frameChanged, parent, [parent]() {
+    if (parent->isVisible()) {
+      parent->update();
+    }
+  }, Qt::UniqueConnection);
+
+  movie->start();
+}
+
+void loadImage(const QString &basePath, QPixmap &pixmap, QSharedPointer<QMovie> &movie, const QSize &size, QWidget *parent) {
+  const QString gifPath = basePath + ".gif";
 
   if (QFileInfo::exists(gifPath)) {
-    QSharedPointer<QMovie> gif(QSharedPointer<QMovie>::create(gifPath, QByteArray(), parent));
-    gif->setCacheMode(QMovie::CacheAll);
-    gif->setScaledSize(size);
-
-    QObject::connect(gif.data(), &QMovie::frameChanged, parent, [parent](int) { parent->update(); }, Qt::UniqueConnection);
-
-    gif->start();
-
-    movie = gif;
+    pixmap = QPixmap();
+    loadGif(gifPath, movie, size, parent);
+  } else {
+    clearMovie(movie, parent);
+    pixmap = QPixmap(basePath + ".png").scaled(size, Qt::KeepAspectRatio, Qt::SmoothTransformation);
   }
 
   parent->update();
-}
-
-void loadImage(const QString &basePath, QPixmap &pixmap, QSharedPointer<QMovie> &movie, const QSize &size, QWidget *parent, Qt::AspectRatioMode aspectRatioMode) {
-  QString gifPath = basePath + ".gif";
-  if (QFileInfo::exists(gifPath)) {
-    loadGif(gifPath, movie, size, parent);
-
-    parent->update();
-  } else {
-    if (!movie.isNull()) {
-      QObject::disconnect(movie.data(), nullptr, parent, nullptr);
-
-      movie->stop();
-      movie.clear();
-    }
-
-    pixmap = QPixmap(basePath + ".png").scaled(size, aspectRatioMode, Qt::SmoothTransformation);
-
-    parent->update();
-  }
 }
 
 void openDescriptions(bool forceOpenDescriptions, std::map<QString, AbstractControl*> toggles) {
